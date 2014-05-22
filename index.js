9d76deb5a3878568ba1c050582b99c15094e9a60
n.b.q.x <nbqxnbq@gmail.com>
2014-05-22 21:40:55 +0900

    treat error

--- index.js
+++ index.js
@@ -12,34 +12,36 @@ module.exports = function(opts){
     s._read = function(){};
     
     c.on('message',function(data){
-      var now = data.val;
-      var diff = _differ(now,_past);
+      if(data!==[]){
+        var now = data.val;
+        var diff = _differ(now,_past);
 
-      if(diff.length!==0){
-        diff.forEach(function(o){
-          var a = o.split('-');
-          var obj = {
-            kimarite: a[2],
-            east: {
-              name: a[0],
-              hoshi: a[1]
-            },
-            west: {
-              name: a[3],
-              hoshi: a[4]
-            }
-          };
-          s.push(obj);
-        });
+        if(diff.length!==0){
+          diff.forEach(function(o){
+            var a = o.split('-');
+            var obj = {
+              kimarite: a[2],
+              east: {
+                name: a[0],
+                hoshi: a[1]
+              },
+              west: {
+                name: a[3],
+                hoshi: a[4]
+              }
+            };
+            s.push(obj);
+          });
+        }
+        _past = now;
       }
-
-      _past = now;
     });
     
     c.send({msg:'start',opts:opts});
 
     return s;
   }catch(e){
+    console.log(e);
     process.exit();
   }
 };
